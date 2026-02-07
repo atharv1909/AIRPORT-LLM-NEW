832a8f02e3fac28b1a0e3dfbd41ad8010156e098
Atharv <atharvrd07@gmail.com>
2026-02-07 17:18:13 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>AIRPORT LLM</h1>
+      <p>Homepage route fixed. Now you can build your UI here.</p>
+    </main>
+  );
+}
